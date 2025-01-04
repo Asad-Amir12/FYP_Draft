26543f8d0e160398b31ed126e7a5725db599d1b0
Asad Amir <asadaries2003@gmail.com>
2025-01-05 00:50:56 +0500

    Scene Loading Added to build settings

---
+++ Assets/Scripts/UI_Managers/LoadingScene.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] Image loadingFill;
+    [SerializeField] private GameObject mainMenuButtons;
+
+    IEnumerator LoadLevelAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log("loadingProgress: " + progress);
+            loadingFill.fillAmount = progress;
+            yield return null;
+        }
+        
+    }
+    public void LoadScene(int sceneIndex)
+    {
+        //gameObject.SetActive(true);
+        loadingScreen.SetActive(true);
+        mainMenuButtons.SetActive(false);
+        StartCoroutine(nameof(LoadLevelAsync), sceneIndex);
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/UI_Managers/MainMenuUI.cs
+++ Assets/Scripts/UI_Managers/MainMenuUI.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public static MainMenuUI _Instance;
+
+    public static MainMenuUI Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                Debug.LogError("MainMenuUI is NULL");
+            }
+            return _Instance;
+        }
+    }
+
+   
+
+    private void Awake()
+    {
+        _Instance = this;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
 }
