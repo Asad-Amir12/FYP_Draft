cf40da87e191c52600139f16804cf07eafb7cf6f
Skyler-Quinn <skylerquinn0@gmail.com>
2025-06-16 04:58:04 +0500

    Sound Effects Added

---
+++ Assets/MyProject/Scripts/Data/Sound Data.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundData
+{
+    public const string Attack = "Player Slash";
+    public const string AttackHit = "Player Slash Enemy";
+    public const string Jump = "Jump";
+    public const string Walk = "Walk";
+    public const string Damage = "Hit";
+    public const string Death = "Death";
+    public const string Roll = "Roll";
+    public const string ButtonClick = "Click";
+    public const string EnemyWalk = "Enemy Walk";
+    public const string EnemyAttack = "Enemy Attack";
+    public const string EnemyDeath = "Enemy Death";
+}
